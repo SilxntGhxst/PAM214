c6c8691db3d751254e8848956f673250f67a1877
Santiago Meneses <sanmenesest17@gmail.com>
2025-09-17 19:04:07 -0600

    Ejercicio 3

--- Practica1/Ejercicios.js
+++ Practica1/Ejercicios.js
@@ -13,3 +13,10 @@ const cuadrado = numero => numero * numero;
 console.log(cuadrado(3));
 console.log(cuadrado(5));
 console.log(cuadrado(10));
+
+const saludoPersonalizado = (nombre, edad) => {
+  return `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+};
+
+const saludoDeSan = saludoPersonalizado("Santiago", 19);
+console.log(saludoDeSan);
