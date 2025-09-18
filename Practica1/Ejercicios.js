f72dd38117fa3356d6d9d9ed6e63eda0212d85d6
Santiago Meneses <sanmenesest17@gmail.com>
2025-09-17 18:58:11 -0600

    Terminación del segundo ejercicio

--- Practica1/Ejercicios.js
+++ Practica1/Ejercicios.js
@@ -6,3 +6,10 @@ let nombreActual = "Ana Maria";
 const saludo = "Hola, " + nombreActual + ". Tienes " + edad + " años.";
 
 console.log(saludo);
+
+
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(3));
+console.log(cuadrado(5));
+console.log(cuadrado(10));
