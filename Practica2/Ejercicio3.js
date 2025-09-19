0c4b65b9fc3a38423e9c709aaed1fee527a35dfc
Santiago Meneses <sanmenesest17@gmail.com>
2025-09-19 16:09:25 -0600

    Ejercicio 3 completado
    Se utilizaron los metodos for each, find y reduce.

---
+++ Practica2/Ejercicio3.js
@@ -0,0 +1,16 @@
+const personas = [
+   { nombre: "Ana", edad: 22 },
+   { nombre: "Luis", edad: 35 },
+   { nombre: "Maria", edad: 28 },
+ ];
+
+//Find
+const buscarluis = personas.find(persona => persona.nombre === "Luis");
+console.log(buscarluis);
+
+//For each
+personas.forEach(persona => console.log(persona.nombre));
+
+//Reduce
+const sumaEdades = personas.reduce((total, persona) => total + persona.edad, 0);
+console.log(sumaEdades);
