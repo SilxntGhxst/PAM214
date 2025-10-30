c746adf9648a34a323df82e41e1df977ca4f833d
Santiago Meneses <sanmenesest17@gmail.com>
2025-10-30 17:11:43 -0600

    ActivityIndicator Completada

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,12 +1,52 @@
-import { Text, StyleSheet, View } from "react-native";
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
 
-export default function ActivityIndicatorScreen() {
+export default function ActivityScreen() {
+  const [cargando, setCargando] = useState(false);
+
+  const iniciarCarga = () => {
+    setCargando(true);
+    setTimeout(() => setCargando(false), 3000);
+  };
+
   return (
-    <View>
-      <Text>Proximamente by Israel & Equipo...</Text>
+    <View style={styles.container}>
+      <Text style={styles.texto}>Presiona para iniciar carga</Text>
+
+      <ActivityIndicator
+        style={styles.indicador}
+        animating={cargando}
+        size="large"
+        color="#129900ff"
+        hidesWhenStopped={true}
+      />
+
+      <View style={styles.botonContainer}>
+        <Button color="red" title="Iniciar carga" onPress={iniciarCarga} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F8F9FA',
+  },
+  texto: {
+    fontFamily: 'Times New Roman',
+    marginBottom: 20,
+    fontSize: 18,
+    color: '#333',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  indicador: {
+    marginVertical: 10,
+  },
+  botonContainer: {
+    marginTop: 30,
+  },
+});
